fee4ac180e926790b3922dc7039bf142469a20c5
tingold <Tom@Toms-MacBook-Pro.local>
2014-03-26 09:57:05 -0400

    continued merge

--- runner/src/main/java/com/boundless/benchmark/tools/Main.java
+++ runner/src/main/java/com/boundless/benchmark/tools/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.boundless.ps.jmxparameterextractor;
+package com.boundless.benchmark.tools;
 
 import java.io.File;
 import javax.xml.transform.Transformer;
