099d5c05a9af2bab0876cc49745bb342a768a59f
tingold <Tom@192.168.1.8>
2014-03-27 10:37:30 -0400

    added DataFinder and DataPackage to find standard test data sets

---
+++ runner/src/test/java/com/boundless/benchmark/data/DataFinderTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.boundless.benchmark.data;
+
+import java.io.File;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Tom
+ */
+public class DataFinderTest {
+    
+    public DataFinderTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of findData method, of class DataFinder.
+     */
+    @Test
+    public void testFindData() 
+    {
+        List<DataPackage> result = DataFinder.findData(new File("/Users/Tom/code/gs-benchmark/data"));
+        System.out.println("found "+result.size()+" data packages");
+    }
+}
